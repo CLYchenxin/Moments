6d9583a9205928ca14dbc3c7e4a64b3600c8c532
CLYchenxin <1577838065@qq.com>
2014-10-16 15:56:51 +0800

    111

---
+++ Moments/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Moments
+//
+//  Created by chenxin on 14-10-16.
+//  Copyright (c) 2014年 chenxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
